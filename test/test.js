db7d0f5c961996a775354c44e330c6557d913b69
hezedu <hezedu@hotmail.com>
2015-09-29 00:54:18 +0800

    review

--- test/test.js
+++ test/test.js
@@ -9,11 +9,12 @@ logMaster.split({
     "nodeApp": "/usr/local/software/node/node_app/log",
     "nodeWap": "/usr/local/software/node/node_wap/log",*/
     //"nodeWechat": "/usr/local/softeware/node-log-master/testtt"
-    "nodeWechat": "D:/duwei/log/test"
+    "nodeWechat": "D:/duwei/test/log"
   },
-  "Suffix":[".log",'.test'],
+  "Suffix":[".log",'.js'],
   "to": __dirname,
-  "Interval": 1000 * 60 ,
+  "Interval": 1000 * 60  ,
   "timeFormat": "yyyy年MM月dd日HH时mm分ss秒",
-  "startTime":'1:1:9'//开始
+  "startTime":'1:1:9',//开始
+  "startTime": h+':'+m+':0'//开始
 });
